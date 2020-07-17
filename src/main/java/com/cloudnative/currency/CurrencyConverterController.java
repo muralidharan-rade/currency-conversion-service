baba4551a1495ec48bf591a6adc611216cc43756
Muralidharan Balanandan <muralidharan.rade@gmail.com>
2020-07-17 15:42:46 +0530

    consume exchange API

--- src/main/java/com/cloudnative/currency/CurrencyConverterController.java
+++ src/main/java/com/cloudnative/currency/CurrencyConverterController.java
@@ -1,10 +1,14 @@
 package com.cloudnative.currency;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConverterController {
@@ -12,7 +16,17 @@ public class CurrencyConverterController {
 	@GetMapping(path = "/currency/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrenyConversionBean getCurrencyConversion(@PathVariable String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
-		return new CurrenyConversionBean(1L, from, to, quantity, new BigDecimal(100), new BigDecimal(100));
+
+		Map<String, String> uriVariables = new HashMap<String, String>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		ResponseEntity<CurrenyConversionBean> responseBean = new RestTemplate().getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrenyConversionBean.class,
+				uriVariables);
+
+		return new CurrenyConversionBean(responseBean.getBody().getId(), from, to, quantity,
+				responseBean.getBody().getMultiple(), quantity.multiply(responseBean.getBody().getMultiple()));
 	}
 
 }
