a8e0ac21f1c3e71be9176e51ef84267c44c429e9
Muralidharan Balanandan <muralidharan.rade@gmail.com>
2020-07-28 14:42:50 +0530

    Changes to connect over Zuul Gateway

--- src/main/java/com/cloudnative/currency/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/cloudnative/currency/CurrencyExchangeServiceProxy.java
@@ -5,11 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service")
-@RibbonClient(name = "currency-exchange-service")
+// @FeignClient(name = "currency-exchange-service") // connect the exchange service directly
+@FeignClient(name = "zuul-api-gateway")
+// @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
+	@GetMapping(path = "/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrenyConversionBean getCurrencyExchangeValue(@PathVariable String from, @PathVariable String to);
 
 }
